e1f2f7a215c7e5e257dc8c46b8c3c212dae01ca6
Dmitry <radiator211@mail.ru>
2022-03-06 18:57:58 +0700

    Initial Commit

---
+++ AwardsCollectionApp/MyAchievement.swift
@@ -0,0 +1,58 @@
+//
+//  MyAchievement.swift
+//  AwardsCollectionApp
+//
+//  Created by Дмитрий Бессонов on 06.03.2022.
+//
+
+import SwiftUI
+
+struct MyAchievement: View {
+    var body: some View {
+        GeometryReader { geometry in
+            let width = geometry.size.width
+            let height = geometry.size.height
+            
+            let size = min(width, height)
+            
+            ZStack {
+                
+                Rectangle()
+                    .cornerRadius(size / 5)
+                    .foregroundColor(Color(red: 0.2, green: 0, blue: 0.4))
+                    .frame(
+                        width: width,
+                        height: height
+                    )
+                
+                
+                Image(systemName: "globe.asia.australia.fill")
+                    .resizable()
+                    .scaleEffect(0.5)
+                    .offset(x: 0, y: 40)
+                    .foregroundColor(.blue)
+                
+                Image(systemName: "tortoise.fill")
+                    .resizable()
+                    .scaleEffect(0.4)
+                    .offset(x: 5, y: -30)
+                    .foregroundColor(.green)
+                
+                Text("За самый быстрый интернет!")
+                    .foregroundColor(.orange)
+                    .bold()
+                    .offset(x: 0, y: size / 1.5)
+                    .lineLimit(2)
+                    .font(.title3)
+                    .frame(width: width)
+            }
+        }
+    }
+}
+
+struct MyAchievement_Previews: PreviewProvider {
+    static var previews: some View {
+        MyAchievement()
+            .frame(width: 200, height: 200)
+    }
+}
